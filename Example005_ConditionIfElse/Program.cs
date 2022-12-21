7efe9e2d9dda0444dcf25bde331f29521b7c9ce7
Yan Voronkov <sub.voronkov@gmail.com>
2022-12-21 19:05:12 +0300

    Add Example005_ConditionIfElse project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") //ToLower переводит введенное значение пользователем Маша в нижний регистр
+{
+	Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+	Console.Write($"Привет, {username}");
+}
