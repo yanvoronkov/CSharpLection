7b0f6e57b263924a7752f8fcb8deb2cb9f00195e
Yan Voronkov <sub.voronkov@gmail.com>
2023-01-11 21:12:10 +0300

    Add Example011_MethodLibrary

---
+++ Example011_MethodLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+	int length = collection.Length;
+	int index = 0;
+	while (index < length)
+	{
+		collection[index] = new Random().Next(1, 10);
+		index++;
+	}
+
+}
+
+void PrintArray(int[] col)
+{
+	int count = col.Length;
+	int position = 0;
+	while (position < count)
+	{
+		Console.WriteLine(col[position]);
+		position++;
+	}
+}
+
+int IndexOf(int[] collection, int find)
+{
+	int count = collection.Length;
+	int index = 0;
+	int position = -1; //поставили -1 имея ввиду, что индекс не найден
+	while (index < count)
+	{
+		if (collection[index] == find)
+		{
+			position = index;
+			break;
+		}
+		index++;
+	}
+	return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
+
