c2756f61b1b852f32a66a6fc8a3bb472208824c1
Yan Voronkov <sub.voronkov@gmail.com>
2023-01-12 08:12:46 +0300

    Add Example012_Methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿// Вид 1 ничего не принимаю и ничего не возвращают
+void Method1()
+{
+	Console.WriteLine("Автор ...");
+}
+
+Method1(); //Вызов метода
+
+// Вид 2 ничего не возвращают, но могут принимать аргументы
+void Method2(string msg)
+{
+	Console.WriteLine(msg);
+}
+
+Method2("Текст сообщения"); //Вызов метода c аргументом
+
+void Method21(string msg, int count)
+{
+	int i = 0;
+	while (i < count)
+	{
+		Console.WriteLine(msg);
+		i++;
+	}
+
+}
+
+Method21("Сообщение", 4); //запуск метода с помощью двух аргументов
+Method21(msg: "Сообщение", count: 4); //запуск метода с явным указанием конкретных аргументов
+Method21(count: 4, msg: "новый текст"); //если явно указываются аргументы, можно их менять местами и не соблюдать последовательность ввода
+
+// Вид 3 что-то возвращают, но ничего не принимают
+
+int Method3()
+{
+	return DateTime.Now.Year;
+
+}
+
+int year = Method3(); //вызов метода с пустыми скобками без аргументов и можно, например, присвоить значение переменной
+Console.WriteLine(year);
+
+// Вид 4 что-то принимают, и что-то возвращают
+
+string Method4(int count, string text)
+{
+	int i = 0;
+	string result = String.Empty;
+	while (i < count)
+	{
+		result = result + text;
+		i++;
+	}
+	return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
